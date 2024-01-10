f7853d7bffd2e84f4b5396b37002db7f6e4ee1be
Khang <khang.nguyenduyvn@gmail.com>
2024-01-10 21:27:53 +0700

    Add adblock recovery message

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -29,6 +29,19 @@ module.exports = {
       {},
       "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-TRC2SF4L67');",
     ],
+    [
+      "script",
+      {
+        async: true,
+        src: "https://fundingchoicesmessages.google.com/i/pub-5904323684803247?ers=1",
+        nonce: "3HRDcENAr3Egc7skT36mnQ",
+      },
+    ],
+    [
+      "script",
+      { nonce: "3HRDcENAr3Egc7skT36mnQ" },
+      "(function () { function signalGooglefcPresent() { if (!window.frames['googlefcPresent']) { if (document.body) { const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe); } else { setTimeout(signalGooglefcPresent, 0); } } } signalGooglefcPresent(); })();",
+    ],
   ],
 
   themeConfig: {
