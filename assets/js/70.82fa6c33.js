da83aa270db219fefafe8248746921ea9ba96bb5
Khang <khang.nguyenduyvn@gmail.com>
2024-04-15 12:17:47 +0000

    Updates

--- assets/js/70.82fa6c33.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[70],{500:function(t,n,s){"use strict";s.r(n);var e=s(13),o=Object(e.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("a-redirect",{attrs:{to:"/about"}})],1)}),[],!1,null,null,null);n.default=o.exports}}]);
