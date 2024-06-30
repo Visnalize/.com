9be06f398e50ecc5af6699c055809c6b93bed638
Khang <khang.nguyenduyvn@gmail.com>
2024-06-30 07:12:56 +0000

    Updates

--- assets/js/55.b5215739.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[55],{460:function(t,i,n){"use strict";n.r(i);var s={props:{path:String},computed:{link(){return"https://brick1100.visnalize.com/#"+this.path}}},r=n(13),e=Object(r.a)(s,(function(){var t=this._self._c;return t("a",{attrs:{href:this.link}},[t("code",[this._v(this._s(this.link))])])}),[],!1,null,null,null);i.default=e.exports}}]);
