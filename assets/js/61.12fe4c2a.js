291a74ee9c2402f4c11b8c385a13e76e3b60ecbd
Khang <khang.nguyenduyvn@gmail.com>
2024-08-31 13:35:36 +0000

    Updates

---
+++ assets/js/61.12fe4c2a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[61],{473:function(t,e,n){"use strict";n.r(e);var o={props:{to:{type:String,required:!0}},beforeMount(){document.location.replace(this.to)}},r=n(13),u=Object(r.a)(o,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);e.default=u.exports}}]);
