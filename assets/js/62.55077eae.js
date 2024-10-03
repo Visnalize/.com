cd6e7d16afa4ddddd08a11e95c5138fc9030237c
Khang <khang.nguyenduyvn@gmail.com>
2024-10-03 04:06:42 +0000

    Updates

---
+++ assets/js/62.55077eae.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[62],{506:function(t,e,n){"use strict";n.r(e);var o={props:{to:{type:String,required:!0}},beforeMount(){document.location.replace(this.to)}},r=n(16),u=Object(r.a)(o,(function(){return(0,this._self._c)("div")}),[],!1,null,null,null);e.default=u.exports}}]);
