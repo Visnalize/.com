76fc00eef63438f15d4adf92416943c8c0ce2ae4
Khang <khang.nguyenduyvn@gmail.com>
2025-01-02 15:34:41 +0700

    Add default OG image, tweak og generator

--- .vitepress/server/ogimage.ts
+++ .vitepress/server/ogimage.ts
@@ -5,10 +5,13 @@ import { getThumURL } from "thum.io";
 import { loadEnv } from "vitepress";
 
 const { VITE_THUM_AUTH_KEY: authKey } = loadEnv("", cwd());
+const defaultOgImage = "/assets/visnalize-og.png";
 
 const getOgImage = async (pagePath: string) => {
+  if (!pagePath) return defaultOgImage;
+
   const publicDir = join(cwd(), "public", "assets", "ogimage");
-  const fileName = (pagePath ? pagePath.replace(/\//g, "-") : "home") + ".png";
+  const fileName = pagePath.replace(/\//g, "-").replace(".html", "") + ".png";
   const filePath = join(publicDir, fileName);
   const publicPath = `/assets/ogimage/${fileName}`;
 
@@ -16,19 +19,27 @@ const getOgImage = async (pagePath: string) => {
     return publicPath;
   }
 
+  const refreshCache = "?m=" + new Date().getMonth(); // add month to force refresh in case maxAge doesn't work
   const thumUrl = getThumURL({
-    url: "noanimate/https://visnalize.com/" + pagePath,
+    url: `noanimate/https://visnalize.com/${pagePath}${refreshCache}`,
     auth: authKey,
     ogImage: true,
+    maxAge: 24 * 7, // 1 week in hours
   });
-  const response = await fetch("https:" + thumUrl);
-  const arrayBuffer = await response.arrayBuffer();
 
-  mkdirSync(publicDir, { recursive: true });
+  try {
+    const response = await fetch("https:" + thumUrl);
+    const arrayBuffer = await response.arrayBuffer();
 
-  writeFileSync(filePath, Buffer.from(arrayBuffer));
+    mkdirSync(publicDir, { recursive: true });
 
-  return publicPath;
+    writeFileSync(filePath, Buffer.from(arrayBuffer));
+
+    return publicPath;
+  } catch (e) {
+    console.error("Failed to fetch OG image for path: " + pagePath);
+    return defaultOgImage;
+  }
 };
 
 export default getOgImage;
